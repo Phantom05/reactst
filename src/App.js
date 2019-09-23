6b5c10392545742e3c792ec3b6b20280e9b688f0
phantom05 <monster2jy@gmail.com>
2019-09-23 23:46:22 +0900

    업데이트 : 리액트 공부 시좍~

--- src/App.js
+++ src/App.js
@@ -2,23 +2,80 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Avatar(props){
+  return (
+    <img className="Avatar" 
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
+}
+
+function UserInfo(props){
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
+
+// function Comment(props){
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <UserInfo user={props.author} />
+//         <div className="Comment-text">
+//           {props.text}
+//         </div>
+//         <div className="Comment-date">
+//           {formatDate(props.date)}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>;
+}
+
+
+// function Clock(props){
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+// }
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()}
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+      <Clock />
     </div>
   );
 }
