9e21c07db08165bcac734c0e8a4f013ce0834faa
phantom05 <monster2jy@gmail.com>
2019-09-24 00:28:08 +0900

    업데이트 : 리액트 공홈 문서 공부

--- src/App.js
+++ src/App.js
@@ -57,6 +57,23 @@ class Clock extends React.Component{
   constructor(props){
     super(props);
     this.state = {date: new Date()}
+
+  }
+
+  // 돔들이 레디가 다되고 클래스들이 root안으로 들어갔으떄
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(), 1000);
+  }
+  componentWillUnmount(){
+    // 언마운트됬을때, DOM에서 삭제됬을때 종료.
+    // event는 이렇게 언마운트 처리를 다 해줘야함. 하나의 앱에서 돌아가기 때문에.
+    clearInterval(this.timerID)
+  }
+  tick(){
+    this.setState({
+      date:new Date()
+    })
   }
   render(){
     return(
@@ -69,6 +86,111 @@ class Clock extends React.Component{
 }
 
 
+
+
+class Toggle extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {isToggleOn:true};
+    // this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick=(e) =>{
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  deleteRow=(bb)=>{
+    console.log(bb);
+  }
+  render(){
+    return(
+      <div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn? "On":"OFF"}
+      </button>
+         <button onClick={this.deleteRow('g')}>Delete Row</button>
+      </div>
+    )
+  }
+}
+
+function UserGreeting(props){
+  return <h1>Welcome back!</h1>
+}
+function GuestGreeting(props){
+  return <h1>Please sign up</h1>
+}
+
+function Greeting(props){
+  const isLoggedIn = props.isLoggedIn;
+  if(isLoggedIn){
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />
+}
+
+function LoginButton(props){
+  return (
+    <button onClick={props.onClick}>Login</button>
+  );
+}
+function LogoutButton(props){
+  return (
+    <button onClick={props.onClick} >
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state ={isLoggedIn:false}
+  }
+
+  handleLoginClick(){
+    this.setState({isLoggedIn:true})
+  }
+
+  handleLogoutClick(){
+    this.setState({isLoggedIn:false})
+  }
+  render(){
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let button = null;
+    if(isLoggedIn){
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    }else{
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+    return(
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
+}
+
+function Mailbox(props){
+  const unreadMessage = props.unreadMessage;
+  return(
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessage.length >0 &&
+      <h2>
+        You has {unreadMessage.length} unread messages.
+      </h2>}
+    </div>
+  )
+}
+
+const messages = ['React','Re: React','Re:Re: React']
 function App() {
   return (
     <div className="App">
@@ -76,6 +198,12 @@ function App() {
       <Welcome name="Cahal" />
       <Welcome name="Edite" />
       <Clock />
+      <Toggle />
+      {/* <button onClick={handleClick}>Activate Lasers</button> */}
+      {/* <a href="#" onClick={handleClick}>Click me</a> */}
+    <Greeting isLoggedIn={true} />
+     <LoginControl />
+      <Mailbox unreadMessage={messages} />
     </div>
   );
 }
